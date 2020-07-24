e78a5d3e8208153a9971ef45e67740c42552eb13
Mitchell McMillan <mitchelldmcmillan@gmail.com>
2020-07-24 12:42:35 +1000

    Upload root folder

---
+++ precache-manifest.0976bc576acfc5a4263f7157aef13330.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e13c17c8ce42c1ae16d945cfbc2bc9c2",
+    "url": "/2a6365ae50b8e1bc1b3b.worker.js"
+  },
+  {
+    "revision": "8c3202864f5efa8bd98b50d7500d2631",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5385ce8a755a0dfa4f9f",
+    "url": "/static/css/main.cb68a4ef.chunk.css"
+  },
+  {
+    "revision": "9b866632fad7e61355aa",
+    "url": "/static/js/2.572f4d09.chunk.js"
+  },
+  {
+    "revision": "d6226ead92df551047503e21c60dd32c",
+    "url": "/static/js/2.572f4d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5385ce8a755a0dfa4f9f",
+    "url": "/static/js/main.e630f999.chunk.js"
+  },
+  {
+    "revision": "f07654458230b62f0d5c",
+    "url": "/static/js/runtime-main.fd1afa18.js"
+  },
+  {
+    "revision": "90d61eb5c66c35269c3719cdc0f633c9",
+    "url": "/static/media/ConnectionII.90d61eb5.woff"
+  },
+  {
+    "revision": "fdbb4654667af3509869f23ff2cf813d",
+    "url": "/static/media/ConnectionII.fdbb4654.ttf"
+  },
+  {
+    "revision": "2f9a8692721ad9514ef661d98e9a1f76",
+    "url": "/static/media/dog-still.2f9a8692.png"
+  }
+]);
