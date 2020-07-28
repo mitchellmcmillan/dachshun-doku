232b9e946e532142688299154f3dd5f118ecacf2
Mitchell McMillan <mitchelldmcmillan@gmail.com>
2020-07-28 21:01:23 +1000

    add reset functionality

---
+++ precache-manifest.af69174fd04fcd8af055c3d3ec4bbb9c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55dedde9c646a23d8bff0d38affc7aca",
+    "url": "/dachshun-doku/6c160d89d42b3de1d4da.worker.js"
+  },
+  {
+    "revision": "ea0df39a300d028e9d9c7f07d0bd1c7f",
+    "url": "/dachshun-doku/index.html"
+  },
+  {
+    "revision": "00a561531dbdd01e07fc",
+    "url": "/dachshun-doku/static/css/main.7fda5f9b.chunk.css"
+  },
+  {
+    "revision": "8a6317241c443b67c53f",
+    "url": "/dachshun-doku/static/js/2.a713b0f9.chunk.js"
+  },
+  {
+    "revision": "b7da0874a4b1930bc9a90c95f87a0cb7",
+    "url": "/dachshun-doku/static/js/2.a713b0f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a561531dbdd01e07fc",
+    "url": "/dachshun-doku/static/js/main.fc80b34b.chunk.js"
+  },
+  {
+    "revision": "a47e8849bc216f586b6f",
+    "url": "/dachshun-doku/static/js/runtime-main.b1c85c4c.js"
+  },
+  {
+    "revision": "90d61eb5c66c35269c3719cdc0f633c9",
+    "url": "/dachshun-doku/static/media/ConnectionII.90d61eb5.woff"
+  },
+  {
+    "revision": "fdbb4654667af3509869f23ff2cf813d",
+    "url": "/dachshun-doku/static/media/ConnectionII.fdbb4654.ttf"
+  },
+  {
+    "revision": "2f9a8692721ad9514ef661d98e9a1f76",
+    "url": "/dachshun-doku/static/media/dog-still.2f9a8692.png"
+  }
+]);
